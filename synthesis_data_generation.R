e8ae13fcfa23c52e1a32e2717ec47990e69bdd55
victorgalmiche <victorgalmiche@gmail.com>
2026-04-09 13:41:14 +0000

    added renv and tested fit w/ SemiMarkov library

--- synthesis_data_generation.R
+++ synthesis_data_generation.R
@@ -17,9 +17,9 @@ P <- P/rowSums(P)
 # Génération de plusieurs chaînes de Markov 
 M <- 5 # Taille  des chaînes
 N <- 500 # Nombre de chaînes
-states <- matrix(0, nrow=N, ncol=M)
+states <- matrix(0, nrow=N, ncol=M+1) # On ajoute une colonne de manière artificielle
 states[,1] <- sample.int(D, size=N, replace=TRUE, prob=alpha)
-for (j in 2:M){
+for (j in 2:(M+1)){
   current_states <- states[, j-1]
   probs <- P[current_states,]
   states[, j] <- apply(probs, 1, function(p) sample.int(D, size=1, prob=p))
@@ -35,4 +35,21 @@ omega <- cbind(a, lambda)
 # Temps de séjour
 sojournTimes <- apply(states, c(1,2), function(s) rgamma(1, shape=a[s], rate=lambda[s]))
 
+
+# DataFrame respectant le format de SemiMarkov
+id <- rep(1:N, each=M)
+state.h <- as.vector(t(states[, 1:M]))
+state.j <- as.vector(t(states[, 2:(M+1)]))
+time <- as.vector(t(sojournTimes[, 1:M]))
+df <- data.frame(id=id, state.h=state.h, state.j=state.j, time=time)
+
+
+mtrans <- matrix("W", nrow=D, ncol=D)
+diag(mtrans) <- FALSE
+
+fit <- semiMarkov(
+  data = df,
+  states = as.character(1:D),
+  mtrans = mtrans
+)
 
